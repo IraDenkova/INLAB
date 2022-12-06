e055783047e63fbac4b064e83db1026c9ffb23c0
IraDenkova <denkovaira@gmail.com>
2022-12-06 23:43:09 +0200

    added 3 sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-  
+
+  const menuIcon = document.querySelector('.menu-passive__btn');
+  if (menuIcon){
+    const menuBody = document.querySelector('.menu-active');
+    menuIcon.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+      menuIcon.classList.toggle('_active');
+      menuBody.classList.toggle('_visible');
+    })
+  }
 });
